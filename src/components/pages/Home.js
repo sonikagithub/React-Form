7f0015a0f4b55faaec3bf60332bc68343bafc876
Pooja Verma <pritamsoni9922@gmail.com>
2024-03-11 13:21:59 +0530

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,24 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <hr />
+      <p>
+        Home Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+        ipsum rerum, unde placeat porro sed natus error voluptatem nam! Deleniti
+        reprehenderit architecto nam temporibus. Non repellendus nisi illum at
+        expedita recusandae omnis veniam dignissimos autem? Sapiente placeat
+        magni nihil repudiandae? Laboriosam ab in sed excepturi odio optio at
+        placeat pariatur! Nam aliquid fuga illum. Aut repudiandae voluptates a
+        aperiam eum at quidem, dicta aliquid nesciunt eos quasi dolores
+        adipisci? Iste quo iusto facere nihil aliquam dicta fuga porro minima
+        repellat, laboriosam fugiat veritatis quisquam ut debitis, neque
+        dolores, obcaecati sit magni eligendi culpa. Sint harum nulla alias
+        atque minima quis.
+      </p>
+      
+    </>
+  );
+};
+
+export default Home;
